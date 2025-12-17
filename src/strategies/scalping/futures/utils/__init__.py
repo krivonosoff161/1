abf5f9e142fcee7d6eb250341a0db7a0e3ff136d
krivonosoff161 <krivonosoffya@yandex.ru>
2025-12-17 23:24:49 +0300

    Дополнительные файлы

--- src/strategies/scalping/futures/utils/__init__.py
+++ src/strategies/scalping/futures/utils/__init__.py
@@ -1,4 +1,5 @@
 """Утилиты для futures-стека (единицы измерения, конвертации и т.п.)."""
 
-from .units import (bps_to_fraction, fraction_to_bps,  # noqa: F401
-                    fraction_to_pct_points, pct_points_to_fraction)
+from .units import fraction_to_bps  # noqa: F401
+from .units import (bps_to_fraction, fraction_to_pct_points,
+                    pct_points_to_fraction)
